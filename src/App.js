2fc1e5e23c8c7f2466ae4e6d59024c78d3408055
Dave Clayton <davedx@gmail.com>
2021-03-29 11:33:07 +0200

    Repro

--- src/App.js
+++ src/App.js
@@ -1,25 +1,31 @@
-import logo from './logo.svg';
-import './App.css';
+import {View, PropTypes} from 'react-view';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
-
-export default App;
+export const App = () => (
+  <View
+    componentName="Button"
+    props={{
+      children: {
+        value: 'Hello',
+        type: PropTypes.ReactNode,
+        description: 'Visible label.',
+      },
+      onClick: {
+        value: '() => alert("click")',
+        type: PropTypes.Function,
+        description: 'Function called when button is clicked.',
+      },
+      disabled: {
+        value: false,
+        type: PropTypes.Boolean,
+        description: 'Indicates that the button is disabled',
+      },
+    }}
+    scope={{
+    }}
+    imports={{
+      'your-button-component': {
+        named: ['Button'],
+      },
+    }}
+  />
+);
